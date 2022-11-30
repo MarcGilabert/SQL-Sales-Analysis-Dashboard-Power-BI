1973ba3742e2f7583c6f5bcfd8739e0e21e4e9bf
MarcGilabert <markgilabert@gmail.com>
2022-11-30 14:26:56 +0100

    first commit

---
+++ DIM_Calendar_v2.sql
@@ -0,0 +1,27 @@
+--Cleaned DIM DateTable--
+
+SELECT 
+  [DateKey], 
+  [FullDateAlternateKey] AS Date,
+  --,[DayNumberOfWeek]
+  [EnglishDayNameOfWeek] AS Day,
+  --,[SpanishDayNameOfWeek]
+  --,[FrenchDayNameOfWeek]
+  --,[DayNumberOfMonth]
+  --,[DayNumberOfYear]
+  [WeekNumberOfYear] AS WeekNr, 
+  [EnglishMonthName] AS Month,
+  LEFT ([EnglishMonthName],3) AS MonthShort, 
+  --,[SpanishMonthName]
+  --,[FrenchMonthName]
+  [MonthNumberOfYear] AS MonthNo, 
+  [CalendarQuarter] AS Quarter, 
+  [CalendarYear] AS Year
+  --,[CalendarSemester]
+  --,[FiscalQuarter]
+  --,[FiscalYear]
+  --,[FiscalSemester]
+FROM 
+  [AdventureWorksDW2019_updated].[dbo].[DimDate]
+  WHERE CalendarYear >= 2019
+
---
+++ DIM_Customer.sql
@@ -0,0 +1,40 @@
+--Cleaned DIM_Customers Table--
+SELECT 
+
+  c.customerkey AS CustomerKey, 
+  --     ,[GeographyKey]
+  --     ,[CustomerAlternateKey]
+  --     ,[Title]
+  c.firstname AS [First Name], 
+  --      ,[MiddleName]
+  c.lastName AS [LastName], 
+  c.firstname + ' ' + lastname AS [Full Name], 
+  -- Combined First and Last name    
+  --      ,[NameStyle]
+  --      ,[BirthDate]
+  --      ,[MaritalStatus]
+  --      ,[Suffix]
+  CASE c.gender WHEN 'M' THEN 'Male' WHEN 'F' THEN 'Female' END AS Gender, --      ,[EmailAddress]
+  --      ,[YearlyIncome]
+  --      ,[TotalChildren]
+  --      ,[NumberChildrenAtHome]
+  --      ,[EnglishEducation]
+  --      ,[SpanishEducation]
+  --      ,[FrenchEducation]
+  --      ,[EnglishOccupation]
+  --      ,[SpanishOccupation]
+  --      ,[FrenchOccupation]
+  --      ,[HouseOwnerFlag]
+  --      ,[NumberCarsOwned]
+  --      ,[AddressLine1]
+  --      ,[AddressLine2]
+  --      ,[Phone]
+  c.datefirstpurchase AS DateFirstPurchase, 
+  --      ,[CommuteDistance]
+  g.city AS [Customer City] --Joined in Customer City from Goography Table 
+FROM 
+  dbo.dimcustomer AS c 
+  LEFT JOIN dbo.dimgeography AS g ON g.geographykey = c.geographykey 
+ORDER BY 
+  CustomerKey ASC --Ordered List by Customers
+
